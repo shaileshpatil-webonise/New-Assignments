02fd94c4db87197c99cb9e970dc1b9d9c785215d
unknown <shailesh.patil@weboniselab.com>
2016-12-20 04:18:55 -0800

    flats

--- Flats/js/custom.js
+++ Flats/js/custom.js
@@ -19,4 +19,12 @@ $(this).parents(".analysisMenu ").find('li').removeClass("active");
 $(this).parents(".analysisMenu li").addClass("active");
 return false;
 });
+$('.content').hide();
+$('.accord li a').click(function(){
+$(this).toggleClass('active')
+$(this).next('.content').stop().slideToggle();
+$(this).parents('li').siblings().find('a').removeClass('active');
+$(this).parents('li').siblings().find('.content').slideUp();
+return false;
+});
 });
