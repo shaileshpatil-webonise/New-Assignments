4c39cf4e66ace3421373472add4e42655b8a54e2
unknown <shailesh.patil@weboniselab.com>
2016-12-29 05:22:18 -0800

    BodyFit

--- Bodyfit/js/custom.js
+++ Bodyfit/js/custom.js
@@ -6,7 +6,7 @@ $(".equimentsc").colorbox({rel:'equiptmentsc'});
 $(".equimentsd").colorbox({rel:'equiptmentsd'});
 $("#click").click(function(){ 
 $('#click').css({"background-color":"#f00", "color":"#fff", "cursor":"inherit"}).text("Open this window again and this message will still be here.");
-return false;
+return false;   
 });
 $("#flip").click(function(){
 $("#panel").slideToggle();
@@ -20,3 +20,29 @@ this.classList.toggle("active");
 this.nextElementSibling.classList.toggle("show");
 }
 }
+(function ($window) {
+//Configure colorbox call back to resize with custom dimensions 
+$.colorbox.settings.onLoad = function() {
+colorboxResize();
+}
+//Customize colorbox dimensions
+var colorboxResize = function(resize) {
+var width = "90%";
+var height = "90%";
+if($(window).width() > 960) { width = "860" }
+if($(window).height() > 700) { height = "630" } 
+$.colorbox.settings.height = height;
+$.colorbox.settings.width = width;
+//if window is resized while lightbox open
+if(resize) {
+$.colorbox.resize({
+'height': height,
+'width': width
+});
+} 
+}
+//In case of window being resized
+$(window).resize(function() {
+colorboxResize(true);
+});
+})(jQuery, this, this.document);
