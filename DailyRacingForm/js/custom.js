e848e0caf04d4e69e6a866b9d8ebcd034203d0cc
unknown <shailesh.patil@weboniselab.com>
2017-01-04 02:07:36 -0800

    drf

--- DailyRacingForm/js/custom.js
+++ DailyRacingForm/js/custom.js
@@ -12,4 +12,36 @@ return false;
 $("#flip").click(function(){
 $("#panel").slideToggle();
 });
+$("#click").click(function(){ 
+$('#click').css({"background-color":"#f00", "color":"#fff", "cursor":"inherit"}).text("Open this window again and this message will still be here.");
+return false;   
 });
+$(".youtube").colorbox({iframe:true, innerWidth:640, innerHeight:390});
+});
+
+(function ($window) {
+//Configure colorbox call back to resize with custom dimensions 
+$.colorbox.settings.onLoad = function() {
+colorboxResize();
+}
+//Customize colorbox dimensions
+var colorboxResize = function(resize) {
+var width = "90%";
+var height = "90%";
+if($(window).width() > 960) { width = "860" }
+if($(window).height() > 700) { height = "630" } 
+$.colorbox.settings.height = height;
+$.colorbox.settings.width = width;
+//if window is resized while lightbox open
+if(resize) {
+$.colorbox.resize({
+'height': height,
+'width': width
+});
+} 
+}
+//In case of window being resized
+$(window).resize(function() {
+colorboxResize(true);
+});
+})(jQuery, this, this.document);
